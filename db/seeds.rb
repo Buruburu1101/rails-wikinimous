6880d289534c1b583bb10e88c77a0527082d6330
Zhen Luo <zhenluo1988@gmail.com>
2023-07-01 17:01:02 +0900

    finished this challenge almost

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'Destroying articles...'
+Article.destroy_all
+puts 'Done!'
+
+puts 'Creating 10 articles...'
+10.times do
+  art = Article.create(
+    title: Faker::Book.title,
+    content: Faker::Quote.famous_last_words
+  )
+  puts "#{art.title} created"
+end
+puts 'Done!'
